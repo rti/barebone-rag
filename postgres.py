a02aefd749982f4fce526cf9aac0ded95cc9495c
Robert Timm <mail@rtti.de>
2024-04-19 20:13:04 +0000

    feat: postgres query functions

--- postgres.py
+++ postgres.py
@@ -1,5 +1,7 @@
 import psycopg
 from psycopg.sql import SQL, Literal
+from typing import List
+from dataclasses import dataclass
 
 _db: psycopg.Connection | None = None
 
@@ -45,3 +47,42 @@ def init(embeddingLength: int):
         ).format(Literal(str(embeddingLength)))
     )
     db.commit()
+
+
+@dataclass
+class Chunk:
+    text:str
+
+
+def get_similar_chunks(embeddingString: str, numberOfResults=5) -> List[Chunk]:
+    cur = get_connection().cursor()
+    cur.execute(
+        "SELECT text FROM chunks ORDER BY embedding <-> %s LIMIT %s;",
+        (
+            embeddingString,
+            numberOfResults,
+        ),
+    )
+    res = cur.fetchall()
+    cur.close()
+
+    return [Chunk(text=r[0]) for r in res]
+
+
+@dataclass
+class Page:
+    title: str
+    text: str
+
+
+def get_pages(limit=10) -> List[Page]:
+    cur = get_connection().cursor()
+    cur.execute(
+        "SELECT title, text FROM pages LIMIT %s;",
+        (limit,),
+    )
+    res = cur.fetchall()
+    cur.close()
+
+    return [Page(title=r[0], text=r[1]) for r in res]
+
