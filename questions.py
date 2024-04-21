095ca8a150b27c17597f55eee5b6bc324bb3c2d6
Robert Timm <mail@rtti.de>
2024-04-21 20:22:57 +0000

    chore(questions): cleanup

--- questions.py
+++ questions.py
@@ -1,23 +1,43 @@
 import models
 import postgres
 
-pages = postgres.get_pages()
+pages = postgres.get_random_pages(1)
 
 system = """\
-You are a generating questions.
-You will be provided with texts. 
-You task is to generate questions, that can be answered by the text given.
+Your task is to write factoid questions and answers given a context.
+Your questions should be answerable with a specific, concise piece of factual information from the context.
+Your questions should be formulated in the same style as questions users could ask in a search engine.
+This means that your questions MUST NOT mention something like "according to the passage" or "context".
 """
 
 for page in pages:
-    prompt = f"""\
-Given the following text:
+    prompt = """\
+Provide your questions and answers in the follwing json structure:
 
-{page.title}
-{page.text}
+{
+    "page_id": "<page_id>",
+    "question": "your question 1",
+    "answer": "your answer to question 1",
+}
 
-Generate 3 questions that this text can answer."""
+""" + f"""
+Now here is the context.
+Page ID: {page.id}
+Context:
+{page.title}\n
+{page.text}\n
 
-    print("Prompt:\n\n" + prompt + "\n\n\n")
+Create one factoid question with answer about the context above in a json structure.
+"""
+
+    # print("Prompt:\n\n" + prompt + "\n\n\n")
+    print("\n*******************************************************************************\n")
+    print(page.title)
+    print("\n*******************************************************************************\n")
+    print(page.text)
+    print("\n*******************************************************************************\n")
     print(models.chat(prompt, system))
+    print("\n*******************************************************************************")
+    print("*******************************************************************************")
+    print("*******************************************************************************")
     print()
