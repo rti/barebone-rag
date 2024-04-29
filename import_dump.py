8c3ba167fa458ff0a0f1089f9df62acb9dec4889
Robert Timm <mail@rtti.de>
2024-04-29 16:50:29 +0000

    feat: embedding prefixes for nomic

--- import_dump.py
+++ import_dump.py
@@ -15,8 +15,8 @@ with postgres.get_connection().cursor() as cur:
 
     for index, row in tqdm(df.iterrows(), total=len(df)):
         title = row['title']
-        text = row['markdown']
         desc = row['description']
+        text = row['markdown']
 
         cur.execute(
             "INSERT INTO pages (title, description, text) VALUES (%s, %s, %s) RETURNING id;",
@@ -25,8 +25,9 @@ with postgres.get_connection().cursor() as cur:
         result = cur.fetchall()
         pageId = result[0][0]
 
-        for c in chunker.chunk(text):
-            embedding = models.embeddingString(c)
+        for c in chunker.chunk(text, chunkSize=256, overlap=32):
+            chunkText = title + "\n" + (desc if desc else "") + "\n" + c
+            embedding = models.embeddingString(chunkText, models.EmbeddingPrefix.DOCUMENT)
             cur.execute(
                 "INSERT INTO chunks (text, embedding, page_id) VALUES (%s, %s, %s);",
                 (c, embedding, pageId),
--- models.py
+++ models.py
@@ -1,16 +1,28 @@
 import ollama
+import enum
 
 _client = None
 
-embeddingsModel = "all-minilm"  
-# embeddingsModel = "nomic-embed-text"  
-# embeddingsModel = "mxbai-embed-large"  
+# embeddingsModel = "all-minilm" # 384 dimensional embeddings model - apache-2.0
+# embeddingsModel = "mxbai-embed-large" # 1024 dimensional embeddings model - apache-2.0
+# embeddingsModel = "znbang/bge:large-en-v1.5-f16" # 1024 dimensoinal embeddings model - MIT
+
+embeddingsModel = "nomic-embed-text"  # 768 dimensional embeddings model - apache-2.0
+
+
+class EmbeddingPrefix(enum.Enum):
+    DOCUMENT = "search_document: "
+    QUERY = "search_query: "
+    NONE = ""
+
 
 # chatModel = "mistral:v0.2"
 chatModel = "zephyr:7b-beta"
 # chatModel = "openchat:7b-v3.5-0106"
 # chatModel = "qwen:0.5b"
 # chatModel = "gemma:7b-v1.1"
+# chatModel = "gemma:2b-v1.1"
+# chatModel = "stablelm2:1.6b"
 # chatModel = "command-r:v0.1"
 
 
@@ -23,14 +35,15 @@ def get_connection():
     return _client
 
 
-def embedding(input: str) -> list[float]:
+def embedding(input: str, prefix=EmbeddingPrefix.NONE) -> list[float]:
     client = get_connection()
-    res = client.embeddings(model=embeddingsModel, prompt=input)
+    prompt = prefix.value + input
+    res = client.embeddings(model=embeddingsModel, prompt=prompt)
     return res["embedding"]  # type: ignore # ollama gets something wrong here
 
 
-def embeddingString(input: str) -> str:
-    e = embedding(input)
+def embeddingString(input: str, prefix=EmbeddingPrefix.NONE) -> str:
+    e = embedding(input, prefix)
     return "[" + ", ".join([str(num) for num in e]) + "]"
 
 
