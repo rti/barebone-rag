d4d9de238a83cf89d3ca577fadf1ae1f9cec22f3
Robert Timm <mail@rtti.de>
2024-04-29 18:26:39 +0000

    feat(repl): combine chunks of similar pages in output

--- repl.py
+++ repl.py
@@ -1,20 +1,34 @@
 import models
 import postgres
 
+
 def print_chunk_info(chunk, rank):
-    print(f"{chunk.title} - {rank}")
+    print(f"{chunk.title} ({rank})")
     print(f"{chunk.description}")
+
 
 def print_results(chunks_with_ranks):
     if len(chunks_with_ranks) == 0:
         print("No results found.")
         return
 
-    for (chunk, rank) in chunks_with_ranks:
-        print("=========================")
-        print_chunk_info(chunk, rank)
+    pages = {}
 
-    print("=========================")
+    # merge chunk-ranks of multiple pages into one
+    for chunk, rank in chunks_with_ranks:
+        existing_chunk = pages.get(chunk.pageId, None)
+        if not existing_chunk:
+            pages[chunk.pageId] = (chunk, rank)
+        else:
+            pages[chunk.pageId] = (existing_chunk[0], min(existing_chunk[1], rank))
+
+    # sort pages by rank and print
+    for p in sorted(pages.values(), key=lambda x: x[1]):
+        print_chunk_info(p[0], p[1])
+        print()
+
+    print()
+
 
 if __name__ == "__main__":
     while True:
